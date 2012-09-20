896e18177adf8fd837eb67ded7a843123286bf3b
Ryan Van Etten <ryanve@gmail.com>
2012-09-19 21:48:50 -0400

    reorganize file structure now that css3base.com is live

---
+++ build/.build.php
@@ -0,0 +1,59 @@
+<?php
+
+header('Content-type: text/css');
+
+parse_str($_SERVER['QUERY_STRING']);
+ 
+if ( is_dir('css') ) $path = 'css/';
+else $path = rtrim(dirname(dirname(__FILE__)), '/') . '/css/';
+
+$build = isset($build) && $build !== '_' ? array_unique(explode(',', $build)) : array(
+	'normalize'
+  , 'h5bp'
+  , 'responsive'
+  , 'plus'
+  , 'custom'
+  , 'helper'
+  , 'print'
+);
+
+$nfo = '/* :::::::::::::::::: '
+. "\n" . ' * @link  css3base.com/build/' . str_replace('.css', '', implode(',', $build)) 
+. "\n" . ' * @date  ' . date('Y-m-d')
+. "\n" . ' * ::::::::::::::::: */';
+
+// after hours of trial and error:
+function css3base_compress ($css) {
+	if ( empty($css) || !is_string($css) ) { return false; }
+	$doc = array();
+	preg_match( '/\/\*![\s\S]+?\*\//', $css, $doc );
+	$doc = isset($doc[0]) ? $doc[0] : '';
+	$css = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css ); // remove comments
+	$css = preg_replace( '/\s+/', ' ', $css ); // whitespace to single spaces
+	$css = preg_replace( '/([\w\-])\s*(,|;|{|})\s+({|}|;|,|\+|\.|\-|!|::)/', '$1$2$3', $css ); // leading spaces
+	$css = preg_replace( '/(,|;|{)\s*([\w\.\-#\s]+)(\:|\}|,)/', '$1$2$3', $css );  // leading spaces
+	$css = preg_replace( '/([a-zA-Z0-9])\s+(!important)/', '$1$2', $css );
+	$css = preg_replace( '/([\w\-,]+)\s*(\:)\s*([\w\-,:]*)/', '$1$2$3', $css ); // `width: 0` to `width:0`
+	$css = preg_replace( '/({|}|;|,||\+|\-|!)\s+(,|;|{|})/', '$1$2', $css );
+	$css = preg_replace( '/([\w\-]),\s+/', '$1,', $css );
+	$css = preg_replace( '/({|})\s+([a-zA-Z0-9\-_]|::|@page|@media)/', '$1$2', $css );
+	$css = preg_replace( '/([\w\-]+\:)(;|\s+)({|})/', '$1$3', $css );
+	return trim($doc . "\n" . trim($css));
+}
+
+$output = array();
+
+foreach ( $build as &$n ) {
+	if ( empty($n) ) { continue; }
+	$n = $path . $n;
+	file_exists($n) || file_exists($n .= '.css') and $css = file_get_contents($n);
+	$css = css3base_compress($css);
+	$n = array_pop(explode('/', $n)); // get filename w/ extension but w/o preceding path
+	//empty($css) or $n = '/* ==start=> ' . $n . ' */' . "\n" . $css . "\n" . '/* <=end== ' . $n . ' */';
+	empty($css) or array_push($output, $css);
+	unset($css);
+}
+
+$output = array_filter($output);
+echo $nfo . "\n\n" . implode("\n\n\n", $output) . "\n\n";
+
---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<!--[if lt IE 9]><html class="oldie ++ rad no-js custom"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="++ rad no-js custom"> <!--<![endif]-->
+<head>
+    <meta charset=utf-8>
+    <link rel=dns-prefetch href=//airve.github.com>
+    <link rel=dns-prefetch href=//cdn.airve.com>
+
+    <title>css3base</title>
+    <!-- 
+        view the source of this entire site
+        @link    github.com/ryanve/css3base
+        @logo    cdn.airve.com/logo/css3base/
+    -->
+    <meta name=viewport content="width=device-width,initial-scale=1.0">
+    <meta name=description content="Modular css builder for base stylesheets.">
+    <meta name=author content="Ryan Van Etten">
+
+    <link rel=stylesheet href="http://css3base.com/build/normalize,fit,plus,rad,helper,print.2012091800.css">
+    <link rel=image_src href="http://css3base.com/apple-touch-icon.png">
+
+    <style>
+        body { max-width:1020px; width:98%; padding:0 1% }
+        a { text-decoration:none }
+        form ul { list-style-type:none; padding-left:0 }
+        form li { display:block; padding: 1em; color:#133; background:#bee }
+        form li:hover { background:#aee }
+        form a, .custom form a { color:#409; font-weight:bold }
+        form a:hover,.custom form a:hover { color:#e30 }
+        form li+li { margin-top: 2px }
+        .file:hover { cursor:pointer }
+        .no-js .file:hover,.oldie .file:hover { cursor:none }
+        summary input { display:inline-block; padding: .382em }
+        #build { padding:.3em 1em; font-weight:bold }
+        @media screen and (min-width:310px) {
+            #logo{ width:160px; height:160px }
+        }
+        #logo:hover { opacity: 0.9; filter: alpha(opacity=90); }
+        #build,.custom #build { display:block; padding:.6em 1em; background:#409; color:#ed3; text-align:center }
+        #build:hover,.custom #build:hover { background: #fa0; color:#409; }
+        #footer{ margin:2em 0;border-bottom:1em solid #ffe }
+        nav ul { padding:0 }
+        nav ul li{display:block;float:left;margin:0 0 1em}
+        nav ul li a{ padding:.5em; background:#ed3; color:#409 !important; margin:0 1px 1em 0 }
+        nav ul li a:hover{ background:#fa0 }
+        .chromeframe { padding: 1em }
+        .chromeframe a { border-bottom: 1px solid #bee }
+        .oldie #build, .oldie input, .oldie .raw { display:none }
+    </style>
+    
+    <script src="http://airve.github.com/js/modernizr/modernizr_shiv.min.js"></script>
+   
+</head>
+<body id="start" data-ymd="<?php echo date('Ymd'); ?>">
+
+    <header id="header">
+        <h1>
+            <a href="http://css3base.com" accesskey="1" rel="home">
+                <img id="logo" src="http://cdn.airve.com/logo/css3base/300/css3base_logo_red_300.png" alt="css3base">
+            </a>
+        </h1>
+    </header>
+    
+    <!--[if lt IE 9]>
+        <p class="chromeframe rad dark">You are using an <strong>outdated</strong> browser. To fully experience this site you must <a href="http://browsehappy.com/">upgrade your browser</a> <b>or</b> <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a>.</p>
+    <![endif]-->
+
+    <noscript>
+        For full functionality of this site it is necessary to enable JavaScript.
+        Here are the <a href="http://www.enable-javascript.com/" target="_blank">
+        instructions how to enable JavaScript in your web browser</a>.
+    </noscript>
+    
+    <form class="++">
+        <ul><!-- maybe a <dl> or <details> here -->
+            <li><input checked type=checkbox data-file="normalize"> <strong class="file" title="&radic;">normalize.css</strong> <small><span class=shape>&#9648;</span> The modern alternative to CSS resets by <a href="https://github.com/necolas/normalize.css">@necolas</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/normalize.css">raw</a></small>
+            <li><input checked type=checkbox data-file="h5bp"> <strong class="file" title="&radic;">h5bp.css</strong> <small><span class=shape>&#9648;</span> Defaults from the <a href="https://github.com/h5bp/html5-boilerplate/tree/master/css">HTML5 Boilerplate (@h5bp)</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/h5bp.css">raw</a></small>
+            <li><input checked type=checkbox data-file="fit"> <strong class="file" title="&radic;">fit.css</strong> <small><span class=shape>&#9648;</span> Base responsive styles for HTML5 elements by <a href="http://github.com/ryanve/css3base">@ryanve</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/fit.css">raw</a></small>
+            <li><input type=checkbox data-file="plus"> <strong class="file" title="&radic;">plus.css</strong> <small><span class=shape>&#9648;</span> Mobile-first <a href="http://en.wikipedia.org/wiki/Fibonacci_number" title="Wikipedia &raquo;">Fibonacci</a> text by <a href="http://github.com/ryanve/css3base">@ryanve</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/plus.css">raw</a></small>
+            <!--<li><input type=checkbox data-file="rad"> <strong class="file"  title="&radic;">rad.css</strong> <small><span class=shape>&#9648;</span> 80s-inspired color scheme by <a href="http://github.com/ryanve/css3base">@ryanve</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/rad.css">raw</a></small>-->
+            <li><input type=checkbox data-file="solarized"> <strong class="file" title="&radic;">solarized.css</strong> <small><span class=shape>&#9648;</span> CSS for implementing <a href="http://ethanschoonover.com/solarized">Solarized</a> colors <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/solarized.css">raw</a></small>
+            <li><input checked type=checkbox data-file="custom"> <strong class="file" title="&radic;">custom.css</strong> <small><span class=shape>&#9648;</span> A placeholder for your custom css. <span class=shape>&#9648;</span> <a href="http://css3base.com/css/custom.css">raw</a></small>
+            <li><input type=checkbox data-file="helper"> <strong class="file" title="&radic;">helper.css</strong> <small><span class=shape>&#9648;</span> Helper classes from the <a href="https://github.com/h5bp/html5-boilerplate/tree/master/css">HTML5 Boilerplate (@h5bp)</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/helper.css">raw</a></small>
+            <li><input type=checkbox data-file="print"> <strong class="file" title="&radic;">print.css</strong> <small><span class=shape>&#9648;</span> Hybrid print styles adapted from <a href="https://github.com/h5bp/html5-boilerplate/tree/master/css">HTML5 Boilerplate (@h5bp)</a> <span class=shape>&#9648;</span> <a class=raw href="http://css3base.com/css/print.css">raw</a></small>
+        </ul>
+        <a id="build" class="++" href="#build" accesskey=u>BUILD</a>
+    </form>
+    
+    <footer id="footer">
+        <nav>
+            <ul>
+                <li><a href="#start" title="up" accesskey="a"><span class=shape>&#9650;</span></a>
+                <li><a href="http://twitter.com/ryanve" title="Twitter: @ryanve" accesskey=r>@ryanve</a>
+                <li><a href="/css/" title="css index" accesskey=i><b>/css</b></a>
+                <li><a href="http://cdn.airve.com/logo/css3base/" title="logo index"><b>/logo</b></a></li>
+                <li><a href="https://github.com/ryanve/css3base" title="repo" accesskey=g>Github</a>
+                <li><a href="https://github.com/ryanve/css3base/issues" title="report an issue" accesskey=9>Issue?</a>
+                <li class="visuallyhidden"><a accesskey=x href="http://html5.validator.nu/?doc=http://css3base.com">validate</a>
+                <li class="visuallyhidden"><a id="end" href="#end" title="down" accesskey=z>&#9660;</a>
+            </ul>
+        </nav>
+    </footer>
+
+    <!-- accesskeys: 
+        1: homepage
+        a: jump up
+        g: github repo
+        i: css index
+        u: build
+        z: jump down
+    -->
+
+<!-- Don't run the builder in IE8- --> 
+<!--[if gt IE 8]><!-->
+    <script src="http://airve.github.com/js/elo/elo.min.js"></script>
+    <script>
+    (function ($, location) {
+
+        $ && $(function ($) {
+
+            var dir = 'http://css3base.com/build/'
+              , $inputs = $('input')
+              , defaultBuildName = '_'
+              , ymd = this.body.getAttribute('data-ymd')
+              , ext = isFinite(ymd) ? '.' + ymd + '.css' : '';
+
+            $('.file').on('click', function () {
+                // make it easier to check the checkboxes
+                var node = this;
+                while ( node = node.previousSibling ) {
+                    if ( 'INPUT' === node.tagName ) {
+                        node.checked = !node.checked;
+                        break;
+                    }
+                }
+            });
+            
+            $('#build').on('click', function () {
+            
+                var files = '';
+
+                $inputs.each(function (el) {
+                    var data;
+                    if ( el && el.checked ) {
+                        if ( data = el.getAttribute('data-file') ) {
+                            files += (files ? ',' : '') + data;
+                        }
+                    }
+                });
+
+                files = files || defaultBuildName;
+                location.href = dir + files + ext; // it works with or w/o the extension
+
+            });
+
+        });
+
+    }(this.elo, location));
+    </script>
+<!--<![endif]-->
+
+<!-- Google Analytics code. In the last line, 
+     change UA-XXXXX-X and hostname to yours:
+--><script>
+(function(w,d,t,l,a,h){
+    if(h&&l.hostname!==h)return;
+    w["_gaq"]=[["_setAccount",a],["_trackPageview"]];
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.async=1;g.src=("https:"==l.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g,s);
+}(this,document,"script",location,"UA-5731919-25","css3base.com"));
+</script>
+
+</body> 
+</html>
